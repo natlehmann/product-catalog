189a5b28ab0f2ce7166d0f01b9716be477474fa2
natlehmann@gmail.com <natlehmann@gmail.com@b01445a8-f680-7de8-885d-cb22de37c3af>
2011-08-11 13:34:30 +0000

    correccion del recupero de productos por categoria

--- src/test/java/com/elpudu/productos/catalogo/dao/ProductDaoTest.java
+++ src/test/java/com/elpudu/productos/catalogo/dao/ProductDaoTest.java
@@ -3,7 +3,9 @@ package com.elpudu.productos.catalogo.dao;
 import java.util.LinkedList;
 import java.util.List;
 
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -15,11 +17,64 @@ import com.elpudu.productos.catalogo.domain.Product;
 
 public class ProductDaoTest extends AbstractPuduTest {
 	
+	private Product product1;
+	private Product product2;
+	private Category category1;
+	private Category category2;
+	private Category categoryBoth;
+	private Category categoryNone;
+	
 	@Autowired
 	private ProductDao productDao;
 	
 	@Autowired
 	private CategoryDao categoryDao;
+	
+	@Before
+	public void init() {
+		category1 = buildCategory("c1");
+		category2 = buildCategory("c2");
+		categoryBoth = buildCategory("cboth");
+		categoryNone = buildCategory("cnone");
+		
+		product1 = buildProduct("p1", "p1", category1, categoryBoth);
+		product2 = buildProduct("p2", "p2", category2, categoryBoth);
+	}
+
+	private Category buildCategory(String name) {
+		Category category = new Category();
+		category.setName(name);
+		category = categoryDao.create(category);
+		
+		return category;
+	}
+
+	private Product buildProduct(String name, String code, Category... categories) {
+		Product product = new Product();
+		product.setName(name);
+		product.setCode(code);
+		
+		product = productDao.create(product);
+		
+		for (Category category : categories) {
+			product.addCategory(category);
+		}
+		
+		product = productDao.update(product);
+		
+		return product;
+	}
+	
+	@After
+	public void cleanup() {
+		productDao.delete(product1);
+		productDao.delete(product2);
+		
+		categoryDao.delete(category1);
+		categoryDao.delete(category2);
+		categoryDao.delete(categoryBoth);
+		categoryDao.delete(categoryNone);
+	}
 
 	@Test
 	public void testCreateProduct() {
@@ -68,5 +123,58 @@ public class ProductDaoTest extends AbstractPuduTest {
 		
 		productDao.delete(product);
 		categoryDao.delete(category);
+	}
+	
+	@Test
+	public void testGetByCategory1() {
+		
+		List<Product> products = productDao.getByCategoryId(category1.getId());
+		Assert.assertEquals(1, products.size());
+		Assert.assertEquals(product1, products.get(0));
+	}
+	
+	@Test
+	public void testGetByCategory2() {
+		
+		List<Product> products = productDao.getByCategoryId(category2.getId());
+		Assert.assertEquals(1, products.size());
+		Assert.assertEquals(product2, products.get(0));
+	}
+	
+	@Test
+	public void testGetByCategory3() {
+		
+		List<Product> products = productDao.getByCategoryId(categoryBoth.getId());
+		Assert.assertEquals(2, products.size());
+		Assert.assertTrue(products.contains(product1));
+		Assert.assertTrue(products.contains(product2));
+	}
+	
+	@Test
+	public void testGetByCategory4() {
+		
+		List<Product> products = productDao.getByCategoryId(categoryNone.getId());
+		Assert.assertTrue(products.isEmpty());
+	}
+	
+	@Test
+	public void testGetByCategoryWithImages() {
+		
+		ImageFile image = new ImageFile();
+		ImageFile smallImage = new ImageFile();
+		product1.setImage(image);
+		product1.setSmallImage(smallImage);
+		product1 = productDao.update(product1);
+		
+		List<Product> products = productDao.getByCategoryId(category1.getId());
+		Assert.assertEquals(1, products.size());
+		Assert.assertEquals(product1, products.get(0));
+		
+		Assert.assertNotNull(products.get(0).getImage().getId());
+		Assert.assertNotNull(products.get(0).getSmallImage().getId());
+		
+		product1.setImage(null);
+		product1.setSmallImage(null);
+		product1 = productDao.update(product1);
 	}
 }
